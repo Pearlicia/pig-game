edb4bf30a5f1db26d2abd7f8017760edfdf4b300
Felicia Ebikon <feliciaebikon@gmail.com>
2022-03-14 07:40:20 +0100

    add function to make scores display random numbers

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+
+var scores, roundScore, activePlayer, dice;
+
+scores = [0,0];
+roundScore = 0;
+activePlayer = 1;
+
+dice = Math.floor(Math.random() * 6) + 1;
+document.querySelector('#current--' + activePlayer).textContent = dice;
+
